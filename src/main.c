a768d5aebad751f6b3d2adeebf1ae1a9945a4629
zukas97 <zuercherlukas97@gmail.com>
2024-07-30 19:27:17 -0400

    added falling asteroid

--- src/main.c
+++ src/main.c
@@ -1,8 +1,10 @@
 //#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <stdbool.h>
+#include <time.h>
 #include "defs.h"
 
 
@@ -17,6 +19,8 @@ int last_frame_time;
 SDL_Surface *rocket_surface;
 SDL_Texture *rocket_texture;
 
+SDL_Surface *asteroid_surface;
+SDL_Texture *asteroid_texture;
 
 //Sprites
 struct Rocket {
@@ -29,6 +33,13 @@ struct Rocket {
 	bool right;
 
 } rocket;
+
+struct Asteroid {
+	int x;
+	int y;
+	int width;
+	int height;
+} asteroid;
 
 
 
@@ -66,8 +77,8 @@ int Init_Win(void) {
 
 void input() {
 	SDL_Event event;
-	int is_pressed = event.key.state == SDL_PRESSED;
-	if (SDL_WaitEvent(&event)){
+	//int is_pressed = event.key.state == SDL_PRESSED;
+	if (SDL_PollEvent(&event)){
 		switch (event.type) {
 			case SDL_QUIT:
 				running = false;
@@ -97,6 +108,14 @@ void input() {
 	}
 }
 
+void summon_asteroid() {
+	int rand_y;
+	srand(time(NULL));
+	rand_y = rand() % 700;
+	asteroid.x = rand_y;
+	asteroid.y = -50;
+}
+
 void update() {
 	int wait_time = FRAME_TIME - (SDL_GetTicks() - last_frame_time);
 
@@ -108,11 +127,10 @@ void update() {
 
 	last_frame_time = SDL_GetTicks();
 
-	if (rocket.left) {
-		rocket.x  = 20 * dtime;
-	}
-	else if (rocket.right) {
-		rocket.x = -20 * dtime;
+	asteroid.y += 5;
+
+	if (asteroid.y >= WIN_HEIGHT) {
+		summon_asteroid();
 	}
 
 }
@@ -120,14 +138,19 @@ void update() {
 void render() {
 
 
+	
 	SDL_SetRenderDrawColor(rend, 0, 0, 0, 255);
 	SDL_RenderClear(rend);
 	
 	rocket_surface = IMG_Load("./images/rocket.bmp");
 	rocket_texture = SDL_CreateTextureFromSurface(rend, rocket_surface);
 	SDL_FreeSurface(rocket_surface);
-	
 
+	asteroid_surface = IMG_Load("./images/asteroid.bmp");
+	asteroid_texture = SDL_CreateTextureFromSurface(rend, asteroid_surface);
+	SDL_FreeSurface(asteroid_surface);
+	
+	
 	SDL_Rect Rocket = {
 		rocket.x,
 		rocket.y,
@@ -135,12 +158,26 @@ void render() {
 		rocket.height,
 	};
 
+	SDL_Rect Asteroid = {
+		asteroid.x,
+		asteroid.y,
+		asteroid.width,
+		asteroid.height,
+	};
+
+	
 	SDL_RenderCopy(rend, rocket_texture, NULL, &Rocket);
+	SDL_RenderCopy(rend, asteroid_texture, NULL, &Asteroid);
+	
 	SDL_RenderPresent(rend); 
+	SDL_RenderClear(rend);
+	SDL_DestroyTexture(asteroid_texture);
+	SDL_DestroyTexture(rocket_texture);
 
 }
 
 void destroy() {
+	SDL_DestroyTexture(asteroid_texture);
 	SDL_DestroyTexture(rocket_texture);
 	SDL_DestroyRenderer(rend);
 	SDL_DestroyWindow(win);
@@ -153,6 +190,10 @@ void setup() {
 	rocket.width = 46;
 	rocket.height = 82;
 	rocket.vel = 0;
+	asteroid.x = 100;
+	asteroid.y = 100;
+	asteroid.width = 50;
+	asteroid.height = 50;
 }
 
 int main() {
