70890388d2194151b500777fdf1e6498760c15c2
zukas97 <zuercherlukas97@gmail.com>
2024-07-31 18:54:09 -0400

    removed unused headers

--- src/main.c
+++ src/main.c
@@ -1,10 +1,8 @@
-//#include <stdlib.h
 #include <SDL2/SDL_render.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
 #include <stdbool.h>
 #include <time.h>
 #include "defs.h"
