962f26e2840bdc042023f4d7579a66dbe0d7c203
zukas <zuercherlukas97@gmail.com>
2024-08-13 13:21:28 -0400

    cleaned up a bit

--- src/win_actions.h
+++
@@ -1,6 +0,0 @@
-//#ifndef WIN_ACTIONS_H
-//#define WIN_ACTIONS_H
-#include <SDL2/SDL.h>
-
-void destroy(SDL_Window *win, SDL_Renderer *rend, SDL_Texture *asteroid_texture, SDL_Texture *rocket_texture);
-//#endif
