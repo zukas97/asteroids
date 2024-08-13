1a8b607bef9f7ac15163972908c1c89c89954d95
zukas <zuercherlukas97@gmail.com>
2024-08-13 13:21:55 -0400

    cleaned up a bit

---
+++ src/actions.c
@@ -0,0 +1,15 @@
+#include <SDL2/SDL.h>
+#include "win_actions.h"
+#include <stdbool.h>
+#include "defs.h"
+#include <SDL2/SDL_image.h>
+
+void destroy(SDL_Window *win, SDL_Renderer *rend, SDL_Texture *asteroid_texture, SDL_Texture *rocket_texture) {
+	SDL_DestroyTexture(asteroid_texture);
+	SDL_DestroyTexture(rocket_texture);
+	SDL_DestroyRenderer(rend);
+	SDL_DestroyWindow(win);
+	SDL_Quit();
+}
+
+
---
+++ src/actions.h
@@ -0,0 +1,6 @@
+//#ifndef WIN_ACTIONS_H
+//#define WIN_ACTIONS_H
+#include <SDL2/SDL.h>
+
+void destroy(SDL_Window *win, SDL_Renderer *rend, SDL_Texture *asteroid_texture, SDL_Texture *rocket_texture);
+//#endif
